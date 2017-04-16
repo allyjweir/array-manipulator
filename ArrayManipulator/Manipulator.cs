68a1c4ab28d915b206acdcb504ffabe9debd9b65
Ally Weir <ally.pcgf@gmail.com>
2017-04-16 21:18:08 +0100

    Rename class and sketch out initial method

---
+++ ArrayManipulator/Manipulator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayManipulator
+{
+    public class Manipulator
+    {
+        /// <summary>
+        /// Splits an array by the divisor provided, returning a 
+        /// <see cref="List{T}"/> of arrays, equally sized. 
+        /// <para>
+        /// Where the original array does not divide equally by the divisor 
+        /// then the remainder will be returned as a shorter array.
+        /// </summary>
+        /// <param name="originalArray">The array of integers to be broken up
+        /// into shorter, equal length arrays.</param>
+        /// <param name="divisor">The number of arrays that should be returned
+        /// by the method.</param>
+        /// <returns>A <see cref="List{T}"/> of <see cref="int[]"/></returns>
+        public static List<int[]> Split(int[] originalArray, int divisor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
