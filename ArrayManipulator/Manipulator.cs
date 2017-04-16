d3ac7c8f2ecf9b0db2949f4686c2a0cc700c76a7
Ally Weir <ally.pcgf@gmail.com>
2017-04-16 22:01:23 +0100

    Add working implementation

--- ArrayManipulator/Manipulator.cs
+++ ArrayManipulator/Manipulator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ArrayManipulator
 {
@@ -17,9 +18,38 @@ namespace ArrayManipulator
         /// <param name="divisor">The number of arrays that should be returned
         /// by the method.</param>
         /// <returns>A <see cref="List{T}"/> of <see cref="int[]"/></returns>
-        public static List<int[]> Split(int[] originalArray, int divisor)
+        public static IEnumerable<IEnumerable<int>> Split(IEnumerable<int> originalArray, int divisor)
         {
-            throw new NotImplementedException();
+            // Check parameters meet requirements of method.
+            if (originalArray.Count() < 1)
+            {
+                throw new ArgumentException("Cannot process an empty array");
+            }
+            if (divisor < 1)
+            {
+                throw new ArgumentException("Divisor must be a positive integer");
+            }
+
+            // Round up the newArrayLength to make sure we grab any remainder
+            // if the originalArray does not divide exactly by the divisor.
+            var newArrayLength =
+                (int)Math.Ceiling((double)originalArray.Count() / divisor);
+
+            var splitArrays = new List<IEnumerable<int>>();
+
+            for (int i = 0; i < divisor; i++)
+            {
+                var newArray = originalArray
+                    .Take(newArrayLength);
+
+                splitArrays.Add(newArray);
+
+                // Reassign originalArray to not include the ones we just 
+                // extracted and placed in our splitArrays List.
+                originalArray = originalArray.Skip(newArrayLength);
+            }
+
+            return splitArrays;
         }
     }
 }
