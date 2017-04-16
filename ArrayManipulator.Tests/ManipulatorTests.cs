0aa1b755f762389af4ecd4851c22da28798c1773
Ally Weir <ally.pcgf@gmail.com>
2017-04-16 21:32:22 +0100

    Add test project with basic tests

---
+++ ArrayManipulator.Tests/ManipulatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ArrayManipulator.Tests
+{
+    public class ManipulatorTests
+    {
+        [Fact]
+        public void DividesEquallyTest()
+        {
+            var expected = new List<int[]> { new int[] { 1, 2 },
+                                             new int[] { 3, 4 },
+                                             new int[] { 5, 6 }};
+            var actual = Manipulator.Split(new int[] { 1, 2, 3, 4, 5, 6 }, 3);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void DividesEquallyWithRemainderTest()
+        {
+            var expected = new List<int[]> { new int[] { 1, 2 },
+                                             new int[] { 3, 4 },
+                                             new int[] { 5 }};
+            var actual = Manipulator.Split(new int[] { 1, 2, 3, 4, 5 }, 3);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AvoidsBadInputArrayTest()
+        {
+            Assert.Throws<ArgumentException>(
+                () => Manipulator.Split(new int[] { }, 3));
+        }
+
+        [Fact]
+        public void AvoidsBadDivisorTest()
+        {
+            Assert.Throws<ArgumentException>(
+                () => Manipulator.Split(new int[] { 1, 2, 3 }, -4));
+        }
+    }
+}
